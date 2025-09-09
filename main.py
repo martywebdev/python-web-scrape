9b6cf890e12a7e7cc6aad191857b6d4b187e2c60
martywebdev <martywebdevelopment@gmail.com>
2025-09-09 09:59:53 +0800

    send email on trigger

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+import smtplib
+import ssl
+import os
+
 import requests
 import selectorlib
+from dotenv import load_dotenv
+
+load_dotenv()
 
 URL = 'https://programmer100.pythonanywhere.com/tours'
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
 
 def scrape(url):
     """Scrape the page source from the URL"""
@@ -11,21 +19,37 @@ def scrape(url):
     source = response.text
     return source
 
+
 def extract(source):
     extractor = selectorlib.Extractor.from_yaml_file('extract.yaml')
     value = extractor.extract(source)['tours']
-    return  value
+    return value
+
 
 def store(extracted):
     with open('data.txt', 'a') as f:
         f.write(extracted + '\n')
 
-def send_email():
-    print("Email was sent!")
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = os.getenv('EMAIL')
+    password = os.getenv('PASSWORD')
+
+    receiver = os.getenv('EMAIL')
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+
 
 def read():
     with open('data.txt', 'r') as f:
         return f.read()
+
 
 if __name__ == '__main__':
     scraped = scrape(URL)
@@ -35,4 +59,4 @@ if __name__ == '__main__':
     if extracted != "No upcoming tours":
         if extracted not in content:
             store(extracted)
-            send_email()
+            send_email(extracted)
